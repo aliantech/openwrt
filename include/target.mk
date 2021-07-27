48891adee1f8cdb17a799f46c214c715f0a45c67
aliantech <info@aliantech.com>
2021-07-27 21:01:42 +0800

    Update target.mk

--- include/target.mk
+++ include/target.mk
@@ -40,6 +40,8 @@ DEFAULT_PACKAGES.nas:=\
 	block-mount \
 	fdisk \
 	lsblk \
+	luci-app-samba4 \
+	luci-app-vsftpd \
 	mdadm
 # For router targets
 DEFAULT_PACKAGES.router:=\
@@ -57,9 +59,9 @@ DEFAULT_PACKAGES.router:=\
 	ppp \
 	ppp-mod-pppoe \
 	luci-newapi block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw \
-	luci luci-app-ddns ddns-scripts_aliyun luci-app-upnp luci-app-autoreboot luci-app-vsftpd luci-app-arpbind luci-app-wol \
-	luci-app-nlbwmon luci-app-banip luci-app-acme acme acme-dnsapi luci-app-commands luci-app-qos \
-	luci-app-samba4 luci-app-uhttpd luci-app-watchcat luci-theme-material
+	luci luci-compat luci-app-ddns ddns-scripts ddns-scripts-services ddns-scripts_aliyun luci-app-upnp luci-app-autoreboot luci-app-arpbind luci-app-wol \
+	luci-app-nlbwmon luci-app-banip luci-app-acme acme acme-dnsapi luci-app-commands luci-app-qos luci-app-smartdns \
+	luci-app-watchcat luci-theme-material luci-app-vlmcsd luci-app-filetransfer luci-app-jd-dailybonus luci-app-rclone rclone rclone-ng rclone-webui-react
 
 ifneq ($(DUMP),)
   all: dumpinfo
