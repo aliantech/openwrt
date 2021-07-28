81088fb4d93bc1ffbec5d24f16439408d5e1fe23
aliantech <info@aliantech.com>
2021-07-28 15:00:38 +0800

    Update target.mk

--- include/target.mk
+++ include/target.mk
@@ -59,8 +59,8 @@ DEFAULT_PACKAGES.router:=\
 	luci-newapi block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw \
 	luci luci-compat luci-app-ddns ddns-scripts ddns-scripts-services luci-app-upnp luci-app-autoreboot luci-app-arpbind luci-app-wol \
 	luci-app-nlbwmon luci-app-banip luci-app-acme acme acme-dnsapi luci-app-commands luci-app-qos luci-app-smartdns \
-	luci-app-watchcat luci-theme-material luci-app-vlmcsd luci-app-filetransfer luci-app-jd-dailybonus luci-app-rclone rclone rclone-ng rclone-webui-react \
-	luci-app-samba4 luci-app-vsftpd luci-nginx default-settings luci-mod-dashboard
+	luci-app-watchcat luci-theme-material luci-app-vlmcsd luci-app-jd-dailybonus luci-app-rclone rclone rclone-ng rclone-webui-react \
+	luci-app-samba4 luci-app-vsftpd luci-nginx default-settings luci-mod-dashboard luci-app-qbittorrent luci-app-familycloud
 
 ifneq ($(DUMP),)
   all: dumpinfo
